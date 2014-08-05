19c088db1c0425739cbc825f9d80ab5555fdae78
Christo Mitov <christo.mitov@gmail.com>
2014-08-05 09:37:02 -0400

    added licensing and link info

--- textract/parsers/odt_parser.py
+++ textract/parsers/odt_parser.py
@@ -1,3 +1,26 @@
+# Using http://nullege.com/codes/show/src@o@p@openerp-server-5.0.0-3@bin@addons@document@content_index.py/25/odt2txt
+
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 import sys
 import zipfile
 import xml.dom.minidom
