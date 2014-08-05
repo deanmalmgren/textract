8f56394e073ddfa800c809e6bfcd1d9e886a3fe4
Christo Mitov <christo.mitov@gmail.com>
2014-08-05 18:38:20 -0400

    added comment

--- textract/parsers/tesseract.py
+++ textract/parsers/tesseract.py
@@ -2,6 +2,9 @@ from ..shell import run
 
 
 def extract(filename, **kwargs):
+    """Tesseract cannot output to stdout directly so a tmp file must be
+    created, read and then removed
+    """
     pipe = run(
         'tesseract %(filename)s tmpout && cat tmpout.txt && rm -f tmpout.txt'
         % locals())
