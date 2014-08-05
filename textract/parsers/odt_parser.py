235a408afd4a1044534dd179b5f00f0bfd2b2dc9
Christo Mitov <christo.mitov@gmail.com>
2014-08-04 23:40:37 -0400

    added odt extension support

---
+++ textract/parsers/odt_parser.py
@@ -0,0 +1,30 @@
+import sys, zipfile, xml.dom.minidom
+import StringIO
+
+def extract(filename, **kwargs) :
+    s = StringIO.StringIO(file(filename).read())
+    odt = OpenDocumentTextFile(s)
+    return odt.toString().encode('ascii','replace')
+
+class OpenDocumentTextFile :
+    def __init__ (self, filepath) :
+        zip = zipfile.ZipFile(filepath)
+        self.content = xml.dom.minidom.parseString(zip.read("content.xml"))
+
+    def toString (self) :
+        """ Converts the document to a string. """
+        buffer = u""
+        for val in ["text:p", "text:h", "text:list"]:
+            for paragraph in self.content.getElementsByTagName(val) :
+                buffer += self.textToString(paragraph) + "\n"
+        return buffer
+
+    def textToString(self, element) :
+        buffer = u""
+        for node in element.childNodes :
+            if node.nodeType == xml.dom.Node.TEXT_NODE :
+                buffer += node.nodeValue
+            elif node.nodeType == xml.dom.Node.ELEMENT_NODE :
+                buffer += self.textToString(node)
+        return buffer
+
