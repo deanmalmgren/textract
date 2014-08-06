f24920185e314297b304b09d7616db4d7044b3f0
Christo Mitov <christo.mitov@gmail.com>
2014-08-06 13:46:39 -0400

    code/comment changes

--- textract/parsers/tesseract.py
+++ textract/parsers/tesseract.py
@@ -2,9 +2,9 @@ from ..shell import run
 
 
 def extract(filename, **kwargs):
-    """Tesseract cannot output to stdout directly so a tmp file must be
-    created, read and then removed
-    """
+    """Extract text from various image file formats using tesseract-ocr"""
+    # Tesseract can't output to console directly so you must first create
+    # a dummy file to write to, read, and then delete
     pipe = run(
         'tesseract %(filename)s tmpout && cat tmpout.txt && rm -f tmpout.txt'
         % locals())
