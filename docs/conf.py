b331aaa1868812d7fddc161aac93ccfc4f539d80
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-08-09 03:37:27 -0500

    hopefully fixed path nonsense for displaying textract -h output

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,16 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath('.'), '..')))
+project_root = os.path.abspath(os.path.join(os.path.abspath('.'), '..'))
+sys.path.insert(0, project_root)
 import textract
+
+# manipulate the python path to properly display the output of
+# textract -h in the command line documentation
+os.environ['PATH'] = ':'.join((
+    os.path.join(project_root, 'bin'),
+    os.environ['PATH'],
+))
 
 # -- General configuration ------------------------------------------------
 
