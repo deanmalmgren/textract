6069f6fa2b1faf2c63ac38d917232f0be398d0b2
Shawn Milochik <shawn@milochik.com>
2014-08-10 19:13:24 -0400

    fixes path so Docker runs for everybody...

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 
 # Run this to create an up-to-date Docker container and run tests.
 
-docker build -t milo/textract . && docker run -v /home/shawn/projects/textract:/textract/ milo/textract
+docker build -t milo/textract . && docker run -v $(pwd)/..:/textract/ milo/textract
