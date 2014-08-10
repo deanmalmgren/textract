52c0f454e6db21e362cb655070e5859560f1650a
Shawn Milochik <shawn@milochik.com>
2014-08-10 13:52:31 -0400

    Resolves issue #38
    Use a different random tempfile for each processing job.
    
    Also updates pylint score from 2.0/10 to 8.46/10.

--- textract/parsers/tesseract.py
+++ textract/parsers/tesseract.py
@@ -1,12 +1,25 @@
+"""
+Process an image file using tesseract.
+"""
+
+import tempfile
+import os
+
 from ..shell import run
 
+def temp_filename():
+    """
+    Return a unique tempfile name.
+    """
+    handle, filename = tempfile.mkstemp()
+    os.close(handle)
+    return filename
 
 def extract(filename, **kwargs):
     """Extract text from various image file formats using tesseract-ocr"""
     # Tesseract can't output to console directly so you must first create
     # a dummy file to write to, read, and then delete
-    stdout, stderr = run(
-        'tesseract %(filename)s tmpout && cat tmpout.txt && rm -f tmpout.txt'
-        % locals()
-    )
+    cmd = 'tesseract %(filename)s {0} && cat {0}.txt && rm -f {0} {0}.txt'
+    temp_name = temp_filename()
+    stdout, _ = run(cmd.format(temp_name) % locals())
     return stdout
