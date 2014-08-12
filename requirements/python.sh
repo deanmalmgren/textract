d1ef8cac1d73a02bde5a54ccd6369ae0e5e723af
Shawn Milochik <shawn@milochik.com>
2014-08-12 14:17:24 -0400

    Updates to requirements scripts.
    An attempt to make it work both with Travis and Docker.

--- requirements/python.sh
+++ requirements/python.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# this needs to work both for vagrant provisioning and for travis
-# builds in a python virtualenv. in the virtual machine provisioning,
+# This needs to work for vagrant, Travis builds, and Docker builds.
+# in a python virtualenv. in the virtual machine provisioning,
 # we're passing the directory this should be run from. in travis-ci,
 # its run from the root of the repository.
-if [ "$#" -eq 1 ]; then
-    cd $1
-    base=""
-else
-    base="/requirements/"
-fi
+base=$(dirname $0)
 
-# install the requirements for this package as well as this module.
-pip install -r ${base}python
+# Install the requirements for this package as well as this module.
+pip install -r $base/python
 pip install .
 
-# install the requirements for this package in development
-pip install -r ${base}python-dev
+# Install the requirements for this package in development
+pip install -r $base/python-dev
--- requirements/run.sh
+++ requirements/run.sh
@@ -2,4 +2,4 @@
 
 # Run this to create an up-to-date Docker container and run tests.
 
-docker build -t milo/textract . && docker run -v $(pwd)/..:/textract/ milo/textract
+docker build -t textract/ubuntu12.04 . && docker run -v $(pwd)/..:/textract/ textract/ubuntu12.04
