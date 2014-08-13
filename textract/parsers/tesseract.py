a5a237ad98b5e351392eaad332153009d6b6a7be
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-08-13 09:37:40 -0500

    removing tesseract version info from output. fixes #48

--- textract/parsers/tesseract.py
+++ textract/parsers/tesseract.py
@@ -19,9 +19,17 @@ def temp_filename():
 
 def extract(filename, **kwargs):
     """Extract text from various image file formats using tesseract-ocr"""
-    # Tesseract can't output to console directly so you must first create
-    # a dummy file to write to, read, and then delete
-    cmd = 'tesseract %(filename)s {0} && cat {0}.txt && rm -f {0} {0}.txt'
+
+    # Tesseract can't output to console directly so you must first
+    # create a dummy file to write to, read, and then
+    # delete. Depending on your Tesseract version, it also may print
+    # tesseract version information to standard output
+    devnull = os.devnull
+    command = (
+        'tesseract %(filename)s {0} > %(devnull)s && '
+        'cat {0}.txt && '
+        'rm -f {0} {0}.txt'
+    )
     temp_name = temp_filename()
-    stdout, _ = run(cmd.format(temp_name) % locals())
+    stdout, _ = run(command.format(temp_name) % locals())
     return stdout
