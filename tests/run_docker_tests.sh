ff93ae2c3b3f8e2705b1a76565cc1c575b969337
Shawn Milochik <shawn@milochik.com>
2014-09-06 11:46:12 -0400

    updates Dockerfile

--- tests/run_docker_tests.sh
+++ tests/run_docker_tests.sh
@@ -2,4 +2,18 @@
 
 # Run this to create an up-to-date Docker container and run tests.
 
-docker build -t textract/ubuntu12.04 . && docker run --rm -v $(pwd)/..:/textract/ textract/ubuntu12.04
+cd $(dirname $0)/..
+base=$(pwd)
+
+image="textract/ubuntu12.04"
+
+cp tests/Dockerfile ./Dockerfile
+
+# Note: For speed, the image won't be automatically rebuilt. If the dependencies
+# change and the existing image is outdated, just delete it with:
+# docker rmi <image name>
+docker images | grep $image || docker build -t $image
+docker run --rm -v $base:/textract/ $image
+
+rm ./Dockerfile
+
