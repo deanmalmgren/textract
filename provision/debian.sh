a6d7900bd88da9d5cd24b81c92d0c4aff89f2829
Shawn Milochik <shawn@milochik.com>
2014-09-08 14:40:59 -0400

    updates debian.sh path stuff

--- provision/debian.sh
+++ provision/debian.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-
-# This needs to work both for vagrant provisioning and for Travis
-# builds in a python virtualenv, each of which have different current
-# working directories when this script is called.
-cd $(dirname $0)/..
+# This needs to work both for Vagrant provisioning and for Travis
+# builds in a Python virtualenv, each of which have different current
+# working directories when this script is called. When run in Vagrant, the
+# script is copied to /tmp and executed from there, passing the original
+# path as the first argument. So deal with that.
+if [ "$1" == "" ]; then
+    # normal
+    cd $(dirname $0)/..
+else
+    # run from /tmp by Vagrant.
+    cd $1
+fi
 base=$(pwd)
 
 # Install all of the dependencies required in the examples.
