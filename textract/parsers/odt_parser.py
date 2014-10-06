ba6f685685f660297187ccb3c7585188f8523687
evfredericksen <evfredericksen@gmail.com>
2014-10-06 03:18:46 -0400

    odt tabs and multiple spaces

--- textract/parsers/odt_parser.py
+++ textract/parsers/odt_parser.py
@@ -1,5 +1,5 @@
 import zipfile
-import xml.dom.minidom
+import xml.etree.ElementTree as ET
 import StringIO
 
 from .utils import BaseParser
@@ -10,28 +10,47 @@ class Parser(BaseParser):
     """
 
     def extract(self, filename, **kwargs):
-        # Inspiration from
-        # https://github.com/odoo/odoo/blob/master/addons/document/odt2txt.py
         with open(filename) as stream:
             zip_stream = zipfile.ZipFile(stream)
-            self.content = xml.dom.minidom.parseString(
-                zip_stream.read("content.xml")
-            )
-
+            self.content = ET.fromstring(zip_stream.read("content.xml"))
         return self.to_string()
 
     def to_string(self):
         """ Converts the document to a string. """
         buff = u""
-        for paragraph in self.content.getElementsByTagName("text:p"):
-            buff += self.text_to_string(paragraph) + "\n"
+        for child in self.content.iter():
+            if child.tag in [self.qn('text:p'), self.qn('text:h')]:
+                buff += self.text_to_string(child) + "\n"
+        # remove last newline char
+        if buff:
+            buff = buff[:-1]
         return buff
 
     def text_to_string(self, element):
         buff = u""
-        for node in element.childNodes:
-            if node.nodeType == xml.dom.Node.TEXT_NODE:
-                buff += node.nodeValue
-            elif node.nodeType == xml.dom.Node.ELEMENT_NODE:
-                buff += self.text_to_string(node)
+        if element.tag == self.qn('text:tab'):
+            buff = "\t"
+            if element.tail is not None:
+                buff += element.tail
+            return buff
+        if element.tag == self.qn('text:s'):
+            buff = u" "
+            if element.get(self.qn('text:c')) is not None:
+                buff *= int(element.get(self.qn('text:c')))
+            if element.tail is not None:
+                buff += element.tail
+            return buff
+        if element.text:
+            buff += element.text
+        for child in element:
+            buff += self.text_to_string(child)
+        if element.tail is not None:
+            buff += element.tail
         return buff
+
+    def qn(self, namespace):
+        nsmap = {
+            'text': 'urn:oasis:names:tc:opendocument:xmlns:text:1.0',
+        }
+        spl = namespace.split(':')
+        return '{{{}}}{}'.format(nsmap[spl[0]], spl[1])
