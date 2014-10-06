b4380162701569e9c295336b8a6933a33b14f6a1
evfredericksen <evfredericksen@gmail.com>
2014-10-06 03:53:50 -0400

    typo fix

--- textract/parsers/odt_parser.py
+++ textract/parsers/odt_parser.py
@@ -11,7 +11,7 @@ class Parser(BaseParser):
 
     def extract(self, filename, **kwargs):
         # Inspiration from
--       # https://github.com/odoo/odoo/blob/master/addons/document/odt2txt.py
+        # https://github.com/odoo/odoo/blob/master/addons/document/odt2txt.py
         with open(filename) as stream:
             zip_stream = zipfile.ZipFile(stream)
             self.content = ET.fromstring(zip_stream.read("content.xml"))
