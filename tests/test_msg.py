6b894c05ebd8efe53454cda921633a23526c210d
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-06-23 05:01:49 -0500

    cleaned up commented out code

--- tests/test_msg.py
+++ tests/test_msg.py
@@ -5,13 +5,3 @@ import base
 
 class MsgTestCase(base.BaseParserTestCase, unittest.TestCase):
     extension = 'msg'
-
-    # # skipping these tests because I can't figure out how to make a damned .msg
-    # # file with the standardized text
-    # @unittest.skip("skipping standardized text test for .msg")
-    # def test_standardized_text_cli(self):
-    #     base.BaseBarserTestCase.test_standardized_text_python(self)
-    #
-    # @unittest.skip("skipping standardized text test for .msg")
-    # def test_standardized_text_python(self):
-    #     base.BaseBarserTestCase.test_standardized_text_python(self)
