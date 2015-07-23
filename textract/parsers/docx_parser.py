fe75d988d5c3e70e03fa643d1306229380f5e048
jsmith-mploir <jesse@mploir.com>
2015-07-23 12:42:05 -0700

    Update docx_parser.py

--- textract/parsers/docx_parser.py
+++ textract/parsers/docx_parser.py
@@ -11,12 +11,12 @@ class Parser(BaseParser):
         text = ""
         document = docx.Document(filename)
 
-        # Extract text from root paragraphs
+        # Extract text from root paragraphs.
         text += '\n\n'.join([
             paragraph.text for paragraph in document.paragraphs
         ])
 
-        # Recursively extract text from root tables
+        # Recursively extract text from root tables.
         for table in document.tables:
             text += self._parse_table(table, text)
 
@@ -25,12 +25,13 @@ class Parser(BaseParser):
     def _parse_table(self, table, text):
         for row in table.rows:
             for cell in row.cells:
-                # For every cell in every row of the table, extract text from child paragraphs
+                # For every cell in every row of the table, extract text from 
+                # child paragraphs.
                 text += '\n\n'.join([
                     paragraph.text for paragraph in cell
                 ])
 
-                # Then recursively extract text from child tables
+                # Then recursively extract text from child tables.
                 for table in cell.tables:
                     text += _parse_table(table, text)
 
