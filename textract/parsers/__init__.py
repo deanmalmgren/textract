8be346f7518a25eaa8f63d79a2609d93f0354886
Alec Koumjian <akoumjian@gmail.com>
2016-08-07 20:31:46 -0700

    prototype file mime detection

--- textract/parsers/__init__.py
+++ textract/parsers/__init__.py
@@ -6,13 +6,8 @@ import os
 import importlib
 
 from .. import exceptions
+from .filetype import detect_filetype
 
-# Dictionary structure for synonymous file extension types
-EXTENSION_SYNONYMS = {
-    ".jpeg": ".jpg",
-    ".htm": ".html",
-    "": ".txt",
-}
 
 # default encoding that is returned by the process method. specify it
 # here so the default is used on both the process function and also by
@@ -30,28 +25,20 @@ def process(filename, encoding=DEFAULT_ENCODING, **kwargs):
     if not os.path.exists(filename):
         raise exceptions.MissingFileError(filename)
 
-    # get the filename extension, which is something like .docx for
-    # example, and import the module dynamically using importlib. This
-    # is a relative import so the name of the package is necessary
-    _, ext = os.path.splitext(filename)
-    ext = ext.lower()
-
-    # check the EXTENSION_SYNONYMS dictionary
-    ext = EXTENSION_SYNONYMS.get(ext, ext)
+    ext = detect_filetype(filename)
 
     # to avoid conflicts with packages that are installed globally
     # (e.g. python's json module), all extension parser modules have
     # the _parser extension
-    rel_module = ext + '_parser'
-    module_name = rel_module[1:]
+    module_name = ext + '_parser'
 
-    # if this module name doesn't exist in this directory it isn't
-    # currently supported
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    if not os.path.exists(os.path.join(this_dir, module_name + '.py')):
+    ## If it can't find the module, it's an unsupported file type
+    try:
+        import_path = 'textract.parsers.' + module_name
+        filetype_module = importlib.import_module(import_path)
+    except ImportError:
         raise exceptions.ExtensionNotSupported(ext)
 
     # do the extraction
-    filetype_module = importlib.import_module(rel_module, 'textract.parsers')
     parser = filetype_module.Parser()
     return parser.process(filename, encoding, **kwargs)
