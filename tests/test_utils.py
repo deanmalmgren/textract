deanmalmgren/textract

commit ce7bc0ecde56c12a908aae7e454a0fa03236d911

Improved subprocess API calls

textract/parsers/utils.py
@@ -70,7 +70,7 @@ class ShellParser(BaseParser):
     `Fabric <http://www.fabfile.org/>`_-like behavior.
     """
 
-    def run(self, command):
+    def run(self, args):
         """Run ``command`` and return the subsequent ``stdout`` and ``stderr``
         as a tuple. If the command is not successful, this raises a
         :exc:`textract.exceptions.ShellError`.
@@ -78,7 +78,7 @@ class ShellParser(BaseParser):
 
         # run a subprocess and put the stdout and stderr on the pipe object
         pipe = subprocess.Popen(
-            command, shell=True,
+            args,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
         )
 
@@ -89,7 +89,7 @@ class ShellParser(BaseParser):
         # if pipe is busted, raise an error (unlike Fabric)
         if pipe.returncode != 0:
             raise exceptions.ShellError(
-                command, pipe.returncode, stdout, stderr,
+                ' '.join(args), pipe.returncode, stdout, stderr,
             )
 
         return stdout, stderr

textract/parsers/audio.py
@@ -51,9 +51,6 @@ class Parser(ShellParser):
         http://www.text2speech.org/,
         with American Male 2 for best results
         """
-        command = (
-            'sox -G -c 1 "%(filename)s" {0}'
-        )
         temp_filename = '{0}.wav'.format(self.temp_filename())
-        self.run(command.format(temp_filename) % locals())
+        self.run(['sox', '-G', '-c', '1', filename, temp_filename])
         return temp_filename

textract/parsers/doc_parser.py
@@ -6,5 +6,5 @@ class Parser(ShellParser):
     """
 
     def extract(self, filename, **kwargs):
-        stdout, stderr = self.run('antiword "%(filename)s"' % locals())
+        stdout, stderr = self.run(['antiword', filename])
         return stdout

textract/parsers/image.py
@@ -17,14 +17,5 @@ class Parser(ShellParser):
         else:
             lang = ''
 
-        # Tesseract can't output to console directly so you must first create
-        # a dummy file to write to, read, and then delete
-        devnull = os.devnull
-        command = (
-            'tesseract "%(filename)s" %(lang)s {0} > %(devnull)s && '
-            'cat {0}.txt && '
-            'rm -f {0} {0}.txt'
-        )
-        temp_filename = self.temp_filename()
-        stdout, _ = self.run(command.format(temp_filename) % locals())
+        stdout, _ = self.run(['tesseract',filename,'stdout',lang])
         return stdout

textract/parsers/pdf_parser.py
@@ -40,13 +40,13 @@ class Parser(ShellParser):
         else:
             layout = ''
         stdout, _ = self.run(
-            'pdftotext %(layout)s "%(filename)s" -' % locals()
+            ['pdftotext', layout, filename, '-']
         )
         return stdout
 
     def extract_pdfminer(self, filename, **kwargs):
         """Extract text from pdfs using pdfminer."""
-        stdout, _ = self.run('pdf2txt.py "%(filename)s"' % locals())
+        stdout, _ = self.run(['pdf2txt.py', filename])
         return stdout
 
     def extract_tesseract(self, filename, **kwargs):
@@ -55,7 +55,7 @@ class Parser(ShellParser):
         base = os.path.join(temp_dir, 'conv')
         contents = []
         try:
-            stdout, _ = self.run('pdftoppm "%s" "%s"' % (filename, base))
+            stdout, _ = self.run(['pdftoppm', filename, base])
 
             for page in sorted(os.listdir(temp_dir)):
                 page_path = os.path.join(temp_dir, page)

textract/parsers/ps_parser.py
@@ -6,5 +6,5 @@ class Parser(ShellParser):
     """
 
     def extract(self, filename, **kwargs):
-        stdout, _ = self.run('pstotext "%(filename)s"' % locals())
+        stdout, _ = self.run(['pstotext', filename])
         return stdout

textract/parsers/rtf_parser.py
@@ -7,6 +7,6 @@ class Parser(ShellParser):
 
     def extract(self, filename, **kwargs):
         # http://superuser.com/a/243089/126633
-        stdout, stderr = self.run('unrtf --text "%(filename)s"' % locals())
+        stdout, stderr = self.run(['unrtf', '--text', filename])
         text_conversion = stdout.split('-'*17+'\n', 1)[-1]
         return text_conversion

commit 2b0e01c7289832019f7fb016c8b06d1758013403

Fix ShellError.is_not_installed()
ShellError.is_not_installed() was broken in ce7bc0ecde56c12a908aae7e454a0fa03236d911
when it stopped running Popen with the "shell=True" argument. Since
it wasn't being run through a shell, it was throwing an OSError instead
of running but exiting with an exit code 127.

This is a minimal change so that the ShellError will still work the
same as before, but if we get an OSError we pass in the 127 exit code.
This way if there are still any scenarios where exit code 127 would
be run by the application, it will still work as before.

textract/parsers/utils.py
@@ -5,6 +5,7 @@ reused in many of the other parser modules.
 import subprocess
 import tempfile
 import os
+import errno
 
 import six
 import chardet
@@ -78,10 +79,18 @@ class ShellParser(BaseParser):
         """
 
         # run a subprocess and put the stdout and stderr on the pipe object
-        pipe = subprocess.Popen(
-            args,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        )
+        try:
+            pipe = subprocess.Popen(
+                args,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            )
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                # File not found.
+                # This is equivalent to getting exitcode 127 from sh
+                raise exceptions.ShellError(
+                    ' '.join(args), 127, '', '',
+                )
 
         # pipe.wait() ends up hanging on large files. using
         # pipe.communicate appears to avoid this issue

tests/test_utils.py
@@ -1,0 +1,17 @@
+import unittest
+from uuid import uuid4
+
+from textract.parsers import utils
+from textract.parsers import exceptions
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_shell_parser_run(self):
+        parser = utils.ShellParser()
+        try:
+            # There shouldn't be a command on the path matching a random uuid
+            parser.run([str(uuid4())])
+        except exceptions.ShellError as e:
+            self.assertTrue(e.is_not_installed())
+        else:
+            self.assertTrue(False, "Expected ShellError")