18053d0d40b0b0c82328d5dd10635bd3c448c1a8
Johannes Weytjens <johannes.weytjens@ugent.be>
2019-07-30 14:40:08 +0200

    replace pstotext with pstoascii

--- textract/parsers/ps_parser.py
+++ textract/parsers/ps_parser.py
@@ -2,9 +2,9 @@ from .utils import ShellParser
 
 
 class Parser(ShellParser):
-    """Extract text from postscript files using pstotext command.
+    """Extract text from postscript files using ps2ascii command.
     """
 
     def extract(self, filename, **kwargs):
-        stdout, _ = self.run(['pstotext', filename])
+        stdout, _ = self.run(['ps2ascii', filename])
         return stdout
